6305735a1a0c1851bd89d32fd42b1a69950dcaec
Tororoo <mrouwassi@gmail.com>
2023-06-15 18:22:29 -0700

    updategit add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-*main - Entry piont
-*
-*Return : Always 0 (Success)
-*/
-int main(viod)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
